696d26dff3f13a4ad121ad6745a58e02612a5113
andrewoma <andrewoma@eaf9f57c-d03c-0410-b010-49d0fc4a82d2>
2008-10-13 10:02:08 +0000

    Update scripts and documentation

--- modules/core/bundle/trunk/src/distribution/README.txt
+++ modules/core/bundle/trunk/src/distribution/README.txt
@@ -10,6 +10,6 @@ $ quokka -version
 Ensure that output matches the version you installed.
 
 NOTE: When you run quokka for the first time, it will create a .quokka directory in your
-$HOME directory and seed it with some default settings.
+$HOME directory (or ~/Library/Preferences/Quokka on OS X) and seed it with some default settings.
 
 See the bundled manual (quokka-manual.pdf) or http://quokka.ws for more information.
--- modules/core/bundle/trunk/src/distribution/bin/quokka.bat
+++ modules/core/bundle/trunk/src/distribution/bin/quokka.bat
@@ -1,12 +1,13 @@
 @echo off
+if "%OS%"=="Windows_NT" setlocal
 @rem =========================================================================
 @rem This script is designed to launch multiple versions of quokka via
-@rem quokka's bootstrapping mechanism. 
+@rem quokka's bootstrapping mechanism.
 @rem Edit the variables below to match your most commonly used configuration
 
-set _DEFAULT_QUOKKA=0.3-ss
+set _DEFAULT_QUOKKA=0.3
 set _DEFAULT_ANT=1.7.1
-set _DEFAULT_OPTS=-Xmx512m
+set _DEFAULT_OPTS=-Xmx1024m
 @rem set JAVACMD=</path/to your most commonly used jdk/java>
 
 @rem =========================================================================
@@ -21,19 +22,22 @@ echo QUOKKA_HOME must be set for Win9x environments
 goto error
 
 :nt
-setlocal
 set _NT=true
 @rem %~dp0 is expanded pathname of the current script under NT
 @rem the for loop evaulates it, effectively removing the ..
 for %%i in ("%~dp0..") do set QUOKKA_HOME=%%~fi
 
 :homeSet
-set _LAUNCHER_CP=%QUOKKA_HOME%\lib\apache.ant_ant-launcher_jar_%_DEFAULT_ANT%.jar
-set _QUOKKA_CP=%QUOKKA_HOME%\lib\apache.ant_ant_jar_%_DEFAULT_ANT%.jar
-set _QUOKKA_CP=%_QUOKKA_CP%;%QUOKKA_HOME%\lib\quokka.bundle_core_jar_%_DEFAULT_QUOKKA%.jar
+set _LAUNCHER_CP=%QUOKKA_HOME%\lib\apache.ant_%_DEFAULT_ANT%_launcher_jar.jar
+set _QUOKKA_CP=%QUOKKA_HOME%\lib\apache.ant_%_DEFAULT_ANT%_ant_jar.jar
+set _QUOKKA_CP=%_QUOKKA_CP%;%QUOKKA_HOME%\lib\quokka.bundle_%_DEFAULT_QUOKKA%_core_jar.jar
 
-if "%QUOKKA_OPTS%" == "" set _QUOKKA_OPTS=%QUOKKA_OPTS%
-if not "%QUOKKA_OPTS%" == "" set _QUOKKA_OPTS=%_DEFAULT_OPTS%
+@rem TODO: Get rid of this dummy value - needs some value or the escaping fails.
+@rem       A trailing space works, but is too easily lost
+set _QUOKKA_OPTS=-Ddummy-opts=x
+if not "%QUOKKA_OPTS%" == "" set _QUOKKA_OPTS=%QUOKKA_OPTS%
+if not "%_DEFAULT_OPTS%" == "" set _QUOKKA_OPTS=%_DEFAULT_OPTS%
+set _QUOKKA_ESC_OPTS=%_QUOKKA_OPTS:"=@quot@%
 
 @rem === Get the command line arguments
 set _QUOKKA_CMD_LINE_ARGS=
@@ -46,7 +50,7 @@ goto getArg
 :argsSet
 if not "%_JAVACMD%" == "" goto runQuokka
 
-@rem === Look for a default java 
+@rem === Look for a default java
 set _JAVACMD=%JAVACMD%
 
 if "%JAVA_HOME%" == "" goto noJavaHome
@@ -58,11 +62,10 @@ goto runQuokka
 if "%_JAVACMD%" == "" set _JAVACMD=java.exe
 
 :runQuokka
-set _QUOKKA_ESC_OPTS=%_QUOKKA_OPTS:"=@quot@%
 set _QUOKKA_STANDARD_OPTS=-Dorg.apache.tools.ant.ProjectHelper=ws.quokka.core.main.ant.ProjectHelper "-Dant.library.dir=%QUOKKA_HOME%\antlib" "-Dquokka.bootstrap.jvmArgs=%_QUOKKA_ESC_OPTS%"
 set _QUOKKA_STANDARD_ARGS=-logger org.apache.tools.ant.NoBannerLogger -main ws.quokka.core.main.ant.QuokkaMain -nouserlib
-rem "%_JAVACMD%" %_QUOKKA_STANDARD_OPTS% %_QUOKKA_OPTS% -classpath "%_LAUNCHER_CP%" "-Dant.home=%QUOKKA_HOME%" org.apache.tools.ant.launch.Launcher %_QUOKKA_STANDARD_ARGS% %QUOKKA_ARGS% -cp "%_QUOKKA_CP%" %_QUOKKA_CMD_LINE_ARGS%
 "%_JAVACMD%" %_QUOKKA_STANDARD_OPTS% %QUOKKA_OPTS% -classpath "%_LAUNCHER_CP%" "-Dant.home=%QUOKKA_HOME%" org.apache.tools.ant.launch.Launcher %_QUOKKA_STANDARD_ARGS% %QUOKKA_ARGS% -cp "%_QUOKKA_CP%" %_QUOKKA_CMD_LINE_ARGS%
+
 if ERRORLEVEL 1 goto error
 goto end
 
@@ -84,4 +87,3 @@ set _QUOKKA_ESC_OPTS=
 if "%_NT%"=="true" endlocal
 
 exit /B %ERROR_CODE%
-
